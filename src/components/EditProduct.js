9c7b87f42fb46670df7c04f175b89487d98e5297
agusahumada <agustinaahumada649@gmail.com>
2021-09-25 20:49:15 -0300

    Initial commit

---
+++ src/components/EditProduct.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditProduct = () => {
+    return ( 
+        <h1>Desde editar producto</h1>
+     );
+}
+ 
+export default EditProduct;
---
+++ src/components/NewProduct.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewProducts = () => {
+    return ( 
+        <h1>Desde nuevo producto</h1>
+     );
+}
+ 
+export default NewProducts;
